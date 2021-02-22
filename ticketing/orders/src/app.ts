b45254ab2cad8f95eb535007421316f7d5362c32
aaronm-2112 <aaron.marroquin96@gmail.com>
2021-02-22 11:14:12 -0800

    Test for submodules

--- ticketing/orders/src/app.ts
+++ ticketing/orders/src/app.ts
@@ -1,39 +1,40 @@
-import express from 'express'
-import 'express-async-errors'; //allows synchronous error handling middleware to be called from inside an asynchronous method with throw keyword
-import { json } from 'body-parser'
-import cookieSession from 'cookie-session'
+import express from "express";
+import "express-async-errors"; //allows synchronous error handling middleware to be called from inside an asynchronous method with throw keyword
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
 
-import { errorHandler, NotFoundError, currentUser } from '@ecomtickets/common'
+import { errorHandler, NotFoundError, currentUser } from "@ecomtickets/common";
 
-import { newOrderRouter } from './routes/new'
-import { showOrderRouter } from './routes/show'
-import { indexOrderRouter } from './routes/index'
-import { deleteOrderRouter } from './routes/delete'
+import { newOrderRouter } from "./routes/new";
+import { showOrderRouter } from "./routes/show";
+import { indexOrderRouter } from "./routes/index";
+import { deleteOrderRouter } from "./routes/delete";
 
-const app = express()
-
+const app = express();
 
 // the ingress controller acts as a proxy; this makes it trusted
-app.set('trust proxy', true)
-app.use(json())
-app.use(cookieSession({
-  signed: false,
-  // secure: process.env.NODE_ENV !== 'test' -- for when https is added
-  secure: false
-}))
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    // secure: process.env.NODE_ENV !== 'test' -- for when https is added
+    secure: false,
+  })
+);
 
 // middleware that sets the users cookie if they are authenticated
-app.use(currentUser)
+app.use(currentUser);
 
-app.use(newOrderRouter)
-app.use(showOrderRouter)
-app.use(indexOrderRouter)
-app.use(deleteOrderRouter)
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
+app.use(deleteOrderRouter);
 
-app.all('*', async (req, res) => {
-  throw new NotFoundError()
-})
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-export { app }
+export { app };
--- ticketing/orders/src/index.ts
+++ ticketing/orders/src/index.ts
@@ -1,65 +1,67 @@
-import mongoose from 'mongoose'
-import { app } from './app'
-import { natsWrapper } from './nats-wrapper'
-import { TicketCreatedListener } from './events/listeners/ticket-created-listener'
-import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener'
-import { ExpirationCompleteListener } from './events/listeners/expiration-complete-listener'
-import { PaymentCreatedListener } from './events/listeners/payment-created-listener'
-
+import mongoose from "mongoose";
+import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
+import { TicketCreatedListener } from "./events/listeners/ticket-created-listener";
+import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listener";
+import { ExpirationCompleteListener } from "./events/listeners/expiration-complete-listener";
+import { PaymentCreatedListener } from "./events/listeners/payment-created-listener";
 
 const start = async () => {
-  console.log("Starting orders service.......")
+  console.log("Starting orders service.......");
   if (!process.env.JWT_KEY) {
-    throw new Error('JWT_KEY must be defined')
+    throw new Error("JWT_KEY must be defined");
   }
 
   if (!process.env.MONGO_URI) {
-    throw new Error('MONGO_URI must be defined')
+    throw new Error("MONGO_URI must be defined");
   }
 
   if (!process.env.NATS_CLIENT_ID) {
-    throw new Error('NATS_CLIENT_ID must be defined')
+    throw new Error("NATS_CLIENT_ID must be defined");
   }
 
   if (!process.env.NATS_URL) {
-    throw new Error('NATS_URL must be defined')
+    throw new Error("NATS_URL must be defined");
   }
 
   if (!process.env.NATS_CLUSTER_ID) {
-    throw new Error('NATS_CLUSTER_ID must be defined')
+    throw new Error("NATS_CLUSTER_ID must be defined");
   }
 
   try {
-    await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL)
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
 
     // it would be nice to integrate into our class but that can be dangerous so we don't
-    natsWrapper.client.on('close', () => {
-      console.log('NATS connection closed!')
-      process.exit()
-    })
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
 
-    process.on('SIGINT', () => natsWrapper.client.close())
-    process.on('SIGTERM', () => natsWrapper.client.close())
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
 
-    new TicketCreatedListener(natsWrapper.client).listen()
-    new TicketUpdatedListener(natsWrapper.client).listen()
-    new ExpirationCompleteListener(natsWrapper.client).listen()
-    new PaymentCreatedListener(natsWrapper.client).listen()
+    new TicketCreatedListener(natsWrapper.client).listen();
+    new TicketUpdatedListener(natsWrapper.client).listen();
+    new ExpirationCompleteListener(natsWrapper.client).listen();
+    new PaymentCreatedListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true
-    })
-    console.log("Connected to mongodb")
-
+      useCreateIndex: true,
+    });
+    console.log("Connected to mongodb");
   } catch (e) {
-    console.error(e)
+    console.error(e);
   }
 
   app.listen(3000, () => {
-    console.log("Listening on port 3000!!!")
-  })
-}
+    console.log("Listening on port 3000!!!");
+  });
+};
 
-start()
+start();
